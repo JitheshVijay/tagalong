d73ad95fbd5a64d93b9b5b56def7aca840996179
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-09-25 00:35:53 +0530

    Delete models directory

--- models/Chat.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
-}, { timestamps: true });
-
-module.exports = mongoose.model('Chat', ChatSchema);
