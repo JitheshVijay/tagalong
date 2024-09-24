de484b8c36ec328831d4f6591c7d22a5922e543a
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-09-25 00:36:08 +0530

    Delete routes directory

--- routes/discovery.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { searchUsers } = require('../controllers/discoveryController');
-
-router.get('/users', auth, searchUsers);
-
-module.exports = router;
